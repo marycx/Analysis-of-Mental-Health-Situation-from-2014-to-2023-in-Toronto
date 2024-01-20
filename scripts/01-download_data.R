efb3df57e0b620f1fed83397597413f0ee9b1430
marycx <68626983+marycx@users.noreply.github.com>
2024-01-20 18:51:06 -0500

    clean the code

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,46 +1,27 @@
 ---
-Purpose: Download and clean data from opendatatoronto # change this
-author: "Mary Cheng"
+Purpose: Download and clean mental health apprehension data from opendatatoronto 
+author: Mary Cheng
 Date: 19 January 2024
 Contact: maryc.cheng@mail.utoronto.ca
 ---
 
-```{r}
-install.packages("gitcreds")
-```
 
 ```{r}
-library(gitcreds)
-```
-
-```{r}
-gitcreds_set()
-```
-
-```{r}
+# Install required packages for data download
 install.packages("tidyverse")
-install.packages("janitor")
 install.packages("dplyr")
-install.packages("tidyr")
 install.packages("opendatatoronto")
-install.packages("knitr")
-install.packages("lubridate")
-install.packages("patchwork")
 ```
 
 ```{r}
 # workspace setup
 library(tidyverse)
-library(janitor)
 library(dplyr)
-library(tidyr)
 library(opendatatoronto)
-library(knitr)
-library(lubridate)
-library(patchwork)
 ```
 
 ```{r}
+# Download data from opentoronto
 # get package
 package <- show_package("mental-health-apprehensions")
 	
@@ -49,7 +30,7 @@ package
 # get all resources for this package
 resources <- list_package_resources("mental-health-apprehensions")
 	
-# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+# identify datastore resources
 datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
 	
 # load the first datastore resource as a sample
