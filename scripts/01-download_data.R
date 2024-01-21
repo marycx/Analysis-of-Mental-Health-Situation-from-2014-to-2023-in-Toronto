401a20ce47c08e77627fc4a6f9f856d785769d49
Mary Cheng <maryshuac@gmail.com>
2024-01-21 17:58:43 -0500

    download data from opentoronto

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -13,32 +13,17 @@ library(opendatatoronto)
 ```
 
 ```{r}
-# Download data from opentoronto
+# download the mental health act apprehension data from opentoronto
 package <- show_package("mental-health-apprehensions")
-	
-package
-	
-# get all resources for this package
+
 resources <- list_package_resources("mental-health-apprehensions")
-	
-# identify datastore resources
-datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
-	
-# load the first datastore resource as a sample
-mental_health_data <- filter(datastore_resources, row_number()==1) %>% get_resource()
-	
+
+mental_health_data <- get_resource(resource ="4a92540b-6504-4b25-827d-4511b8b9f3d9")
 mental_health_data
 ```
 
 ```{r}
-# Since this csv file is too huge, I can only include 32000 entries in the csv file. Therefore, I decide to do some basic selection here regarding the years. I am going to select 2012 - 2016 to analyze.
-mental_health_data <- mental_health_data %>% filter(OCC_YEAR == "2015" | OCC_YEAR == "2016" | OCC_YEAR == "2017")
-```
-
-```{r}
 # Save data
-write.csv(x = mental_health_data,
+write_csv(x = mental_health_data,
           file = "inputs/data/unedited_data.csv")
 ```
-
-## 
