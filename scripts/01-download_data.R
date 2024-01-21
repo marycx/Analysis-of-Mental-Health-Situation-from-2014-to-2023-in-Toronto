b27db24089c3d0bde5231592b4024d9a70b32a09
Mary Cheng <maryshuac@gmail.com>
2024-01-21 01:32:37 -0500

    download data from opentoronto

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,6 +1,6 @@
 ---
-title: "Download mental health apprehension dataset from opentoronto"
-author: "Mary Cheng"
+Purpose: Download and clean mental health apprehension data from opendatatoronto 
+author: Mary Cheng
 Date: 19 January 2024
 Contact: maryc.cheng@mail.utoronto.ca
 ---
@@ -38,12 +38,9 @@ mental_health_data
 ```
 
 ```{r}
-# Since I am using Posit Cloud and this csv file is huge, Posit Cloud will only include 32000 entries. Therefore, I decide to do some basic selection here regarding the years. I am going to select 2012 - 2016 to analyze.
-mental_health_data <- mental_health_data %>% filter(OCC_YEAR == "2011" | OCC_YEAR == "2012" | OCC_YEAR == "2013" | OCC_YEAR == "2014" | OCC_YEAR == "2015" | OCC_YEAR == "2016")
-```
-
-```{r}
 # Save data
 write.csv(x = mental_health_data,
           file = "inputs/data/unedited_data.csv")
 ```
+
+## 
